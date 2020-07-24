2fa790b5f07ce89e8989be1b813556fccf3d43d2
Gujing199301 <1165509917@qq.com>
2020-07-24 12:55:55 +0800

    ceshi

---
+++ case/test.py
@@ -0,0 +1,18 @@
+# noinspection PyUnresolvedReferences
+import pytest
+
+import unittest
+
+# noinspection PyUnresolvedReferences
+import allure
+
+
+class TestAdd(unittest.TestCase):
+
+    @allure.step(title='测试登录')
+    def test_01(self):
+        print('登录')
+
+    @allure.step(title='测试充值')
+    def test_02(self):
+        print('充值')
