9d7024aaa433154e4facba55e06bd1674f729ef0
Gujing199301 <46989399+Gujing199301@users.noreply.github.com>
2020-07-24 17:21:22 +0800

    Update test.py

--- case/test.py
+++ case/test.py
@@ -11,12 +11,10 @@ class TestAdd(unittest.TestCase):
 
     @allure.step(title='测试登录')
     def test_01(self):
-
-        allure.attach('点击登录，输入用户名密码，登录')
+        
         print('登录')
 
     @allure.step(title='测试充值')
     def test_02(self):
 
-        allure.attach('使用钱海充值')
         print('充值')
