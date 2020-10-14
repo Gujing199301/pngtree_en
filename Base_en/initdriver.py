28282f97a9506b7f17e1b0d94c71760c890571b1
Gujing199301 <46989399+Gujing199301@users.noreply.github.com>
2020-10-14 17:02:21 +0800

    Update initdriver.py

--- Base_en/initdriver.py
+++ Base_en/initdriver.py
@@ -3,8 +3,12 @@ from selenium import webdriver
 
 def get_driver():
 
-    # 创建浏览器对象
-    driver = webdriver.Chrome()
+    chromeOptions = webdriver.ChromeOptions()
+
+    # 设置代理
+    chromeOptions.add_argument("--proxy-server=http://127.0.0.1:1080")
+    # 一定要注意，=两边不能有空格，不能是这样--proxy-server = http://202.20.16.82:10152
+    driver = webdriver.Chrome(chrome_options=chromeOptions)
 
     # 返回浏览器驱动对象
     return driver
