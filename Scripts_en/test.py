0199b12a39bf1a8a5112f85c79047a66544d170a
Gujing199301 <1165509917@qq.com>
2020-10-14 15:45:40 +0800

    首次提交

---
+++ Scripts_en/test.py
@@ -0,0 +1,24 @@
+# import os,sys
+# from time import sleep
+#
+# sys.path.append(os.getcwd())
+# from Base_en.initdriver import get_driver
+# import requests
+#
+#
+#
+# class Test:
+#
+#     def test(self):
+#
+#         self.driver = get_driver()
+#         url = 'https://pngtree.com/'
+#         self.driver.get(url)
+#         self.driver.maximize_window()
+#         sleep(3)
+
+ 
+
+
+
+
